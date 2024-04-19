e4cc168456a4f4bd9fcd4790fa4ac14f7a034c48
Tanuj Guha Thakurta <tanuj@tangi0.com>
2024-04-19 16:16:18 +0100

    Added login and authentication logic

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
+import os.path
 from typing import List
 
 import uvicorn
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Form, HTTPException, Response, Request
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse, RedirectResponse, FileResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
-
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from fastAPI.app.src.database import Base, engine
 from fastAPI.app.src.dependancies import get_db
 from fastAPI.app.src.domain.customer.models import Customer
@@ -19,6 +23,11 @@ from fastAPI.app.src.domain.items.schemas import *
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+templates = Jinja2Templates(directory=os.path.join("src/static"))
 
 @app.get("/items")
 async def get_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
@@ -42,7 +51,7 @@ async def delete_item(item_id: int, db: Session = Depends(get_db)):
     return delete_item_service(db=db, item_id=item_id)
 
 
-@app.get("/customer")
+@app.get("/customers")
 async def get_customers(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
     return get_customers_service(db=db, skip=skip, limit=limit)
 
@@ -65,6 +74,36 @@ async def delete_customer(email_id: str, db: Session = Depends(get_db)):
 @app.get("/customer/validate/{email_id}")
 async def validate_customer(email_id: str, password: str, db: Session = Depends(get_db)):
     return validate_customer_service(db=db, email_id=email_id, password=password)
+
+
+@app.post("/customer/login")
+async def customer_login(username: str = Form()):
+    return {"email_id": username}
+
+
+@app.post("/token")
+async def login(response: Response, request: Request, form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    try:
+        if validate_customer_service(email_id=form_data.username, password=form_data.password, db=db):
+            customer = jsonable_encoder(get_customer_service(db=db, email_id=form_data.username))
+            access_token = get_access_token(customer)
+            # response = RedirectResponse(url="src/static/login_success.html")
+            response.set_cookie(key="access_token", value=f"Bearer {access_token}")
+            # return response
+            # return {"access_token": access_token, "token_type": "bearer"}
+            # return FileResponse("src/static/login_success.html")
+            return templates.TemplateResponse(request, "login_success.html")
+    except:
+        raise HTTPException(
+            status_code=400,
+            detail="Incorrect details"
+        )
+
+
+@app.post("/login_test")
+async def login_test(token=Depends(oauth2_scheme)):
+    print("in login test", token)
+    return {"token": token}
 
 
 Base.metadata.create_all(bind=engine)
--- app/src/domain/customer/service.py
+++ app/src/domain/customer/service.py
@@ -1,8 +1,20 @@
+import datetime
+import os
+
 import bcrypt
+from fastapi import HTTPException, Depends
+from fastapi.encoders import jsonable_encoder
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from fastAPI.app.src.domain.customer.models import Customer
 from fastAPI.app.src.domain.customer.schemas import CustomerBase
+import jwt
+from jwt import PyJWTError
+
+SECRET_KEY = "abcd"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 20
 
 
 def get_customer_service(db: Session, email_id: str):
@@ -16,7 +28,6 @@ def get_customers_service(db: Session, skip: int = 0, limit: int = 100):
 def create_customer_service(db: Session, customer=CustomerBase):
     customer_dict = customer.dict()
     customer_dict['hashed_password'] = bcrypt.hashpw(customer.password.encode('utf-8'), bcrypt.gensalt(6))
-    print(customer_dict['hashed_password'])
     customer_dict.pop('password')
     new_customer = Customer(**customer_dict)
     db.add(new_customer)
@@ -33,5 +44,27 @@ def delete_customer_service(db: Session, email_id: str):
 def validate_customer_service(db: Session, email_id: str, password: str):
     customer = get_customer_service(db, email_id=email_id)
     hashed_pwd = customer.__dict__.get('hashed_password')
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_pwd)
+    is_validated_customer = bcrypt.checkpw(password.encode('utf-8'), hashed_pwd)
+    return is_validated_customer
 
+
+def create_access_token(data: dict, expires_delta):
+    to_encode = data
+    expire = datetime.datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_access_token(data: dict):
+    expires_delta = datetime.timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data,expires_delta)
+    return access_token
+
+
+def authenticate(self, token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        print("payload: ", payload)
+    except PyJWTError:
+        raise HTTPException(status_code=401, detail="Authentication failed, invalid or expired token")
